6560cecd190b47c4a40fee3ce0285c17c939c154
TRJMajika <101942468+TRJMajika@users.noreply.github.com>
2022-10-11 04:09:30 +0200

    Dashboard has all answers

---
+++ src/components/MyDashboard.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Navbar from "./navbar";
+import "../components/navbar.css";
+import firebase from "../firebase/index";
+import { useEffect, useState } from 'react';
+
+
+const Dashboard = () => {
+  const [myAnswers, setMyAnswers] = useState([]);
+
+  const ref = firebase.firestore().collection("UAnswers")
+
+  function getMyAnswers() {
+      ref.onSnapshot((querySnapshot) => {
+          const items = [];
+          querySnapshot.forEach((doc) => {
+              items.push(doc.data());
+          });
+          setMyAnswers(items);
+      })
+  }
+
+  useEffect(() => {
+      getMyAnswers();
+  });
+  return (
+    <React.Fragment>
+     <div>
+     {myAnswers.map((myanswer) => (
+                <>
+                    <div key={myanswer.doc_id}>
+                        <h5>Question : <p> {myanswer.u_question}</p> </h5>
+                        <h5>Answer : <p> {myanswer.u_answer}</p> </h5>
+                        <hr/>
+                    </div>
+                </>
+            ))}
+
+     </div>
+
+    </React.Fragment>
+    
+  );
+};
+
+export default Dashboard;
