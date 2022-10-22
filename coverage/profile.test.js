b752b4354807b006a0be05493a6515934bef33a8
Lethabo <lethaboletswalo07@gmail.com>
2022-10-22 22:39:20 +0200

    all users

--- coverage/profile.test.js
+++ coverage/profile.test.js
@@ -1,9 +1,11 @@
-import { render } from '@testing-library/react';
-import Profile from './profile';
+import React from "react";
+import { waitFor } from "@testing-library/react";
 
+import { render } from "./utils/testing";
 
+import Profile from "./components/profile";
 
-test('renders the profile', () => {
-  render(<Profile />);
-
+test("Render Profile page", async () => {
+  const { getByText, findByText } = render(<Profile />, { route: "/profile" });
+  await findByText(/Leanne Graham/i);
 });
