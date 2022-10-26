be3a1037bec1e4b01feb2f59f5064b6f38daa643
OwinoMuMBO <sowino2@gmail.com>
2022-10-26 10:17:13 +0200

    update alert

--- src/components/UsersT.js
+++ src/components/UsersT.js
@@ -12,8 +12,8 @@ function UsersT() {
     const [allUsers, setUsers] = useState([]);
     const ref = firebase.firestore().collection("Users");
 
-    // const [myFriends, setFriends] = useState([]);
-    // const refF = firebase.firestore().collection("Friends");
+    const [myFriends, setFriends] = useState([]);
+    const refF = firebase.firestore().collection("Friends");
 
     const [requestedBy_Email, setMyemail] = useState('');
     const [requestedBy_id, setMyId] = useState('');
@@ -43,19 +43,40 @@ function UsersT() {
         AllUsers();
     });
 
+    function myFirends() {
+        refF.where('requestedBy_Email', '==', requestedBy_Email).onSnapshot((querySnapshot) => {
+            const items = [];
+            querySnapshot.forEach((doc) => {
+                items.push(doc.data());
+            });
+            setFriends(items);
+        })
+    }
+
+    useEffect(() => {
+        myFirends();
+    });
+
+    console.log(myFriends.length)
+
+    for (let i=0; i<myFriends.length; ++i){
+        for (let j=0; j<allUsers.length; ++j){
+            if (myFriends[i].requestTo_STN === allUsers[j].studentNumRef){
+                allUsers[j].u_id = "true";
+            }else{
+                allUsers[j].u_id = "false"; 
+            }
+        }
+    }
+
     const friend_doc_id = uuidv4() ;
     const ref2 = firebase.firestore().collection('Friends').doc(friend_doc_id) ;
-    function handleAddFriend(friend,user){
+    function handleAddFriend(friend){
         ref2.set(friend)
-        user.u_friends = friend.requestedBy_Email
-        ref.doc(user.users_doc_id).update(user) 
-
-        alert("Friend Request Added")
         Swal.fire({
             icon: 'success',
             title: 'ADDED',
             text: 'New user Added as Friend.',
-            
           })
     }
 
@@ -67,7 +88,7 @@ function UsersT() {
                     {allUsers.map((user) => (
 
                         <div key={user.studentNumRef}>
-                            {user.u_friends!==requestedBy_Email && <div className='userBox'>
+                            {user.u_id==="false" && <div className='userBox'>
                                 <img className='userImage'
                                     src="https://th.bing.com/th/id/R.77f5794e2eb49f7989b8f85e92cfa4e0?rik=FPingw5xw%2fAHXA&pid=ImgRaw&r=0"
                                 />
@@ -76,14 +97,10 @@ function UsersT() {
                                     <h4 className='firstN'>{user.firstnameRef} {user.lastnameRef} </h4>
                                     <h4 className='studentN'>{user.studentNumRef}</h4>
                                 </div>
-                                <button className='friendbtn' onClick={() => handleAddFriend({requestedBy_Email, requestedBy_id, requestTo_u_id: user.u_id, requestTo_name: user.firstnameRef, requestTo_STN: user.studentNumRef, requestTo_Username: user.usernameRef},
-                                    {users_doc_id: user.users_doc_id, emailRef: user.emailRef, firstnameRef: user.firstnameRef, lastnameRef: user.lastnameRef, passwordRef: user.passwordRef, studentNum: user.studentNumRef, u_created: user.u_created, u_id: user.u_id,
-                                        u_image: user.u_image, usernameRef: user.usernameRef, u_friends: user.u_friends}
-                                    )}
-                                    > Add Friend </button>
+                                <button className='friendbtn' onClick={() => handleAddFriend({requestedBy_Email, requestedBy_id, requestTo_u_id: user.u_id, requestTo_name: user.firstnameRef, requestTo_STN: user.studentNumRef, requestTo_Username: user.usernameRef})}> Add Friend </button>
                             </div>}
 
-                            {user.u_friends===requestedBy_Email && <div className='userBox'>
+                            {user.u_id==="true" && <div className='userBox'>
                                 <img className='userImage'
                                     src="https://th.bing.com/th/id/R.77f5794e2eb49f7989b8f85e92cfa4e0?rik=FPingw5xw%2fAHXA&pid=ImgRaw&r=0"
                                 />
